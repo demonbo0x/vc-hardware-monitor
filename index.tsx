30ad2c1bbe13d46b90f722d386239e1378dba0ae
demonbo0x <mahmoud29118@gmail.com>
2026-06-15 03:22:22 +0300

    Refactor imports and clean up code structure

--- index.tsx
+++ index.tsx
@@ -1,16 +1,14 @@
-import { definePlugin } from "vencord/plugins";
-import { React } from "vencord/webpack/common";
-import { after } from "vencord/patcher";
-import { findByName } from "vencord/webpack";
+import definePlugin from "@utils/types";
+import { React } from "@webpack/common";
+import { after } from "@lib/patcher";
+import { findByName } from "@webpack";
 
-// ده الكومبوننت اللي هيترسم في الشاشة (الـ Widget)
 function HardwareWidget() {
     const [memory, setMemory] = React.useState("0 MB");
 
     React.useEffect(() => {
         const updateMem = () => {
             try {
-                // ديسكورد مبني على Chromium، فبنقدر نقرأ استهلاك الرام المباشر
                 const perfMemory = (window.performance as any).memory;
                 if (perfMemory) {
                     const usedMB = (perfMemory.usedJSHeapSize / 1024 / 1024).toFixed(1);
@@ -21,7 +19,6 @@ function HardwareWidget() {
             }
         };
 
-        // تحديث القراءة كل ثانيتين
         updateMem();
         const interval = setInterval(updateMem, 2000);
         return () => clearInterval(interval);
@@ -49,14 +46,12 @@ function HardwareWidget() {
 export default definePlugin({
     name: "Hardware Monitor",
     description: "مراقب أداء الجهاز (يعرض استهلاك الرامات المباشر)",
-    authors: [{ name: "YourName", id: "000000000000" }], // تقدر تحط الـ ID بتاعك هنا
+    authors: [{ name: "YourName", id: "000000000000" }],
     
     start() {
-        // بنبحث عن لوحة حساب المستخدم اللي تحت عشان نحقن الكود فيها
         const Account = findByName("Account");
         if (Account) {
             after("default", Account, (args, res) => {
-                // بنحط الـ Widget بتاعنا بأمان تام من غير ما نكسر كود ديسكورد
                 if (res?.props?.children && Array.isArray(res.props.children)) {
                     res.props.children.unshift(React.createElement(HardwareWidget));
                 }
@@ -64,7 +59,5 @@ export default definePlugin({
         }
     },
 
-    stop() {
-        // Vencord بيلغي التعديلات أوتوماتيك لما تقفل البلاجن
-    }
+    stop() {}
 });
